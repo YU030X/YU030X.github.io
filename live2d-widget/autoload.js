df739fcec76f057fa2db2404e4d85c945ccf5190
yuuxxx22 <3146162840@qq.com>
2025-09-13 12:18:47 +0800

    Site updated: 2025-09-13 12:18:47

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -37,8 +37,7 @@ if (screen.width >= 768) {
 			//apiPath: "https://live2d.fghrsh.net/api/",
 			//cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/",
 			cdnPath: "https://npm.elemecdn.com/akilar-live2dapi@latest/",
-			tools: ["asteroids", "switch-model", "switch-texture", "info", "quit"],
-			drag: True
+			tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "info", "quit"]
 		});
 	});
 }
